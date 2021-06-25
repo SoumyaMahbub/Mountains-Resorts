2881557d8ca14d08367b82bf7665337948b24d83
SoumyaMahbub <soumyabd2004@gmail.com>
2021-06-25 12:49:26 +0600

    fixing up app.js 2

---
+++ src/Components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+function Main() {
+    return (
+        <div className="main">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Main
---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <button className="">This is a button</button>
+        </div>
+    )
+}
+
+export default Navbar

46354a906899f8da0b397390611677e4d92e0963
SoumyaMahbub <soumyabd2004@gmail.com>
2021-06-30 13:03:20 +0600

    font-awesome and more responsive 2

---
+++ src/Components/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Menu() {
+    return (
+        <div id="menu">
+            <a className="d-block py-3 thin-border text-gold" href="#">All Rooms</a>
+            <a className="d-block py-3 thin-border text-gold" href="#">Schedule</a>
+            <a className="d-block py-3 thin-border text-gold" href="#">Customers</a>
+            <a className="d-block py-3 thin-border text-gold" href="#">Contact Us</a>
+        </div>
+    )
+}
+
+export default Menu
